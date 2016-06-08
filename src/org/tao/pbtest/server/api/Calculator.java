34e407c6d0e8196f7a600f35add92584eeaaa2bf
luyu91 <luyu1@jd.com>
2016-06-08 11:24:09 +0800

    new file:   src/org/tao/pbtest/ipc/Server.java

---
+++ src/org/tao/pbtest/server/api/Calculator.java
@@ -0,0 +1,6 @@
+package org.tao.pbtest.server.api;
+
+public interface Calculator {
+   public int add(int a, int b);
+   public int minus(int a, int b);
+}
---
+++ src/org/tao/pbtest/server/api/CalculatorPB.java
@@ -0,0 +1,6 @@
+package org.tao.pbtest.server.api;
+
+import org.tao.pbtest.proto.Calculator.CalculatorService.BlockingInterface;
+
+public interface CalculatorPB extends BlockingInterface {
+}
